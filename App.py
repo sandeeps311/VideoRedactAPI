d1fdd835641e7fcca7f88bd30b72063472ed8715
Powered Automation <69073137+PoweredAutomation@users.noreply.github.com>
2021-02-04 14:01:29 +0530

    Update App.py

--- App.py
+++ App.py
@@ -1,8 +1,6 @@
-import glob
+
 import json
 import os
-
-import shutil
 import time
 import bleedfacedetector as fd
 import boto3
@@ -68,7 +66,6 @@ def get_uniqueimage():
 
 def compare_images():
     i = 0
-    print(i)
     try:
         for img in os.listdir( r"Media/faces" ):
             try:
@@ -278,219 +275,6 @@ async def create_upload_file(background_tasks: BackgroundTasks, file: UploadFile
     return videoData
 
 
-# @app.post("/detecthead")
-# def upload_detectface(item: models.detectfaceitem):
-#     path = item.path
-#     print(path)
-#     vs = cv2.VideoCapture( path )
-#     frame_width = int( vs.get( 3 ) )
-#     frame_height = int( vs.get( 4 ) )
-#     frame_number = vs.get( cv2.CAP_PROP_FRAME_COUNT )
-#     h = int( vs.get( cv2.CAP_PROP_FRAME_HEIGHT ) )
-#     w = int( vs.get( cv2.CAP_PROP_FRAME_WIDTH ) )
-#     fps = int( vs.get( cv2.CAP_PROP_FPS ) )
-#     print( 'frame count', frame_number )
-#     print( 'frame per second', fps )
-#     seconds = int( frame_number / fps )
-#     print( seconds )
-#     count = 0
-#     video_time = str( datetime.timedelta( seconds=seconds ) )
-#     print( video_time )
-#     list = []
-#     AllList = []
-#     size = (frame_width, frame_height)
-#     result = cv2.VideoWriter( 'filename.avi',
-#                               cv2.VideoWriter_fourcc( *'MJPG' ),
-#
-#                               10, size )
-#     while True:
-#         # grab the frame from the threaded video stream and resize it
-#         # to have a maximum width of 400 pixels
-#         ret, frame = vs.read()
-#         try:
-#             frame = imutils.resize( frame, width=500 )
-#
-#             # grab the dimensions of the frame and then construct a blob
-#             # from it
-#             (h, w) = frame.shape[:2]
-#             blob = cv2.dnn.blobFromImage( frame, 1.0, (300, 300),
-#                                           (104.0, 177.0, 123.0) )
-#
-#             # pass the blob through the network and obtain the face detections
-#             net.setInput( blob )
-#             detections = net.forward()
-#
-#             # loop over the detections
-#             for i in range( 0, detections.shape[2] ):
-#                 # extract the confidence (i.e., probability) associated with
-#                 # the detection
-#                 confidence = detections[0, 0, i, 2]
-#
-#                 # filter out weak detections by ensuring the confidence is
-#                 # greater than the minimum confidence
-#                 if confidence > 0.5:
-#                     # compute the (x, y)-coordinates of the bounding box for
-#                     # the object
-#                     count += 10
-#                     vs.set( 1, count )
-#                     frametime = count / fps
-#                     box = detections[0, 0, i, 3:7] * np.array( [w, h, w, h] )
-#                     (startX, startY, endX, endY) = box.astype( "int" )
-#
-#                     # extract the face ROI
-#                     face = frame[startY:endY, startX:endX]
-#
-#                     resized = cv2.resize(face, (36, 44), interpolation=cv2.INTER_AREA)
-#
-#                     cv2.imwrite( r'C:/Users/Ajinkya/Desktop/All Data/Recovered data 01-07 07_46_29/GitProject/VideoRedact/public/assets/header_img/' + str( math.ceil( frametime * 100 ) / 100 ) + '.png', resized )
-#                     data = {
-#                         "id": uuid.uuid4(),
-#                         "object": 'C:/Users/Ajinkya/Desktop/All Data/Recovered data 01-07 07_46_29/GitProject/VideoRedact/public/assets/header_img/' + str( math.ceil( frametime * 100 ) / 100 ) + '.png',
-#                         "time": math.ceil( frametime * 100 ) / 100,
-#                         "auto": True,
-#                         "Manual": None
-#
-#                     }
-#                     list.append( data )
-#                     # cv2.waitKey( 1 )
-#
-#                     # check to see if we are applying the "simple" face
-#                     # blurring method
-#                     if 'simple' == "simple":
-#                         face = anonymize_face_simple( face, factor=3.0 )
-#                     # otherwise, we must be applying the "pixelated" face
-#                     # anonymization method
-#                     else:
-#                         face = anonymize_face_pixelate( face,
-#                                                         blocks=20 )
-#
-#                     # store the blurred face in the output image
-#                     frame[startY:endY, startX:endX] = face
-#
-#             count += 10  # i.e. at 30 fps, this advances one second
-#             vs.set( 1, count )
-#         except:
-#             print( 'error' )
-#             break
-#
-#     # do a bit of cleanup
-#     vs.release()
-#     result.release()
-#     cv2.destroyAllWindows()
-#
-#     # subprocess.run(f"C:/Users/Ajinkya/Desktop/All Data/Recovered data 01-07 07_46_29/GitProject/VideoRedactAPI/ffmpeg-N-100581-ga454a0c14f-win64-gpl-shared-vulkan/bin/ffmpeg.exe -i test-1_2.mp4 -vn audio_only.wav")
-#
-#     ffmpeg_extract_audio(path, "test.wav", bitrate=3000, fps=44100)
-#
-#     framerate = 0.1
-#
-#     r = sr.Recognizer()
-#     with sr.AudioFile("test.wav") as source:
-#         # reads the audio file. Here we use record instead of
-#         # listen
-#         audio = r.record(source)
-#
-#         # decoder = r.recognize_sphinx(audio, show_all=False)
-#         #
-#         # print([(seg.word, seg.start_frame / framerate) for seg in decoder.seg()])
-#
-#     try:
-#         print("The audio file contains: " + r.recognize_google(audio))
-#         transcript = r.recognize_google(audio)
-#         videoData = {
-#             "id": uuid.uuid4(),
-#             "faces": list,
-#             "transcript": transcript,
-#             "editedVideoPath": ""
-#         }
-#
-#     except sr.UnknownValueError:
-#         print("Google Speech Recognition could not understand audio")
-#         videoData = {
-#             "id": uuid.uuid4(),
-#             "faces": list,
-#             "transcript": "Google Speech Recognition could not understand audio",
-#             "editedVideoPath": "",
-#         }
-#
-#     except sr.RequestError as e:
-#         print("Could not request results from Google Speech Recognition service; {0}".format(e))
-#         videoData = {
-#             "id": uuid.uuid4(),
-#             "faces": list,
-#             "transcript": "Could not request results from Google Speech Recognition service; {0}".format(e),
-#             "editedVideoPath": ""
-#         }
-#
-#     # AllList.append(videoData)
-#
-#     # with open('FaceData.json') as file:
-#     #     file.write(AllList)
-#
-#
-#     return videoData
-#
-# @app.post("/redactfaces")
-# async def redact_face_audio(item: models.redactfaceitem):
-#     print(item.data)
-#     time.sleep(5)
-#     return {"message": "success"}
-#
-# @app.get("/myAllVideos/{user_id}")
-# def myAllVideos(user_id: str):
-#     time.sleep(2)
-#     if user_id == "abc":
-#         # data = [
-#         #     {
-#         #         "id": uuid.uuid4(),
-#         #         "user_id": user_id,
-#         #         "video_name": "RRR Trailer",
-#         #         "video_url": "./assets/UploadedVideo/rrr.mp4",
-#         #         "upload_datetime": datetime.datetime.now(),
-#         #         "thumbnail": "./assets/VideoCover/videocover.png",
-#         #         "is_head_detect": True,
-#         #         "is_transcription": True,
-#         #         "is_edited": True,
-#         #         "is_download": True,
-#         #         "message": "processing"
-#         #     },
-#         #     {
-#         #         "id": uuid.uuid4(),
-#         #         "user_id": user_id,
-#         #         "video_name": "Motivation video",
-#         #         "video_url": "./assets/UploadedVideo/test-1_4.mp4",
-#         #         "upload_datetime": datetime.datetime.now(),
-#         #         "thumbnail": "./assets/VideoCover/videocover1.png",
-#         #         "is_head_detect": True,
-#         #         "is_transcription": True,
-#         #         "is_edited": True,
-#         #         "is_download": True,
-#         #         "message": "success"
-#         #     }
-#         # ]
-#
-#         with open('videoData.json') as f:
-#             data = json.load(f)
-#
-#         return data
-#     else:
-#         data = [
-#             {
-#                 "message": "Invalid user"
-#             }
-#         ]
-#         return data
-#
-#
-# @app.post('/getFaceData')
-# def get_face_data(item: models.redactfaceitem):
-#     time.sleep(2)
-#     print(item.data)
-#     with open('FaceData.json') as f:
-#         data = json.load(f)
-#
-#     return data
-
 
 @app.get( '/getVideoData/{user_id}' )
 async def get_video_data(user_id: str, video_id: str):
@@ -589,15 +373,17 @@ async def get_video_data(user_id: str, video_id: str):
 
 
 @app.post( '/downloadRedactedVideo' )
-async def download_redacted_video(item: models.downloadRedactedVideo):
+async def download_redacted_video(item: models.downloadVideo):
     Aws_access_key_id = 'AKIAIFWF3UATSC6JEWBA'
     Aws_secret_access_key = '4Jd0MizjQFaJJamOuEsGsouEMQOfTLBqWsPeK9L9'
-    
+    # bucketName = 'original-video'
+    #
+    # region = 'us-east-2'
     s3 = boto3.client( 's3',
                        aws_access_key_id=Aws_access_key_id,
                        aws_secret_access_key=Aws_secret_access_key,
                        )
-    #this is new
+
     try:
         video_url = s3.generate_presigned_url(
             ClientMethod='get_object',
@@ -764,13 +550,14 @@ def read_video(item):
 
 
 @app.post( '/getEditedVideo' )
-async def get_edited_video(background_tasks: BackgroundTasks,item: models.downloadVideo):
+async def get_edited_video(background_tasks: BackgroundTasks,item: models.getEditedVideo):
     Aws_access_key_id = 'AKIAIFWF3UATSC6JEWBA'
     Aws_secret_access_key = '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'
     s3 = boto3.client( 's3',
                        aws_access_key_id=Aws_access_key_id,
                        aws_secret_access_key=Aws_secret_access_key,
                        )
+    print(f'Media/{item.video_name}')
     with open( f'Media/{item.video_name}', 'wb' ) as f:
         s3.download_fileobj( 'original-video', f'{item.user_id}/video/{item.video_id}/{item.video_name}', f )
         f.close()
